a47323fc78a7ce5f06e7461acac965e60a60b19e
LeKhai-KLB <khaileklb1111@gmail.com>
2023-07-17 22:22:26 +0700

    server/refactor: create user with hashedPassword

--- server/src/features/users/users.service.ts
+++ server/src/features/users/users.service.ts
@@ -1,18 +1,27 @@
 import { Injectable } from "@nestjs/common";
 import { CreateUserDto } from "./dto/create-user.dto";
-// import { UpdateUserDto } from "./dto/update-user.dto";
 import { Repository } from "typeorm";
 import { InjectRepository } from "@nestjs/typeorm";
 import { User } from "./entities/user.entity";
+import * as bcrypt from "bcrypt";
+import { PaginateConfig } from "nestjs-paginate";
 
 @Injectable()
 export class UsersService {
+  private paginateConfig: PaginateConfig<User>;
   constructor(
     @InjectRepository(User) private readonly userRepository: Repository<User>
   ) {}
 
   async create(createUserDto: CreateUserDto) {
-    const newUser = this.userRepository.create(createUserDto);
+    const { password, email, username } = createUserDto;
+    const salt = await bcrypt.genSalt();
+    const hashedPassword = await bcrypt.hash(password.trim(), salt);
+    const newUser = this.userRepository.create({
+      email,
+      username,
+      password: hashedPassword,
+    });
     await this.userRepository.save(createUserDto);
     return newUser;
   }
@@ -21,7 +30,7 @@ export class UsersService {
   //   return `This action returns all users`;
   // }
 
-  // findOne(id: number) {
+  // findOne(id: string) {
   //   return `This action returns a #${id} user`;
   // }
 
