58ae23e32144bacabd2b8521931cb3a39d949f0b
LeKhai-KLB <khaileklb1111@gmail.com>
2023-07-24 22:49:02 +0700

    feat: add cookie feature

--- server/src/main.ts
+++ server/src/main.ts
@@ -1,12 +1,34 @@
 import { NestFactory } from "@nestjs/core";
 import { AppModule } from "./app.module";
 import { ValidationPipe } from "@nestjs/common";
-import { APP_HOST, APP_PORT } from "./common/constants";
+import {
+  APP_HOST,
+  APP_PORT,
+  APP_SECRET,
+  ENV,
+  RUNTIME_ENV,
+} from "~common/constants";
 import { DocumentBuilder, SwaggerModule } from "@nestjs/swagger";
+import cookieParser from "cookie-parser";
+import session from "express-session";
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  app.enableCors();
   app.useGlobalPipes(new ValidationPipe());
+  app.use(cookieParser());
+  app.use(
+    session({
+      secret: APP_SECRET,
+      saveUninitialized: false,
+      resave: false,
+      cookie: {
+        httpOnly: true,
+        sameSite: "lax",
+        secure: RUNTIME_ENV === ENV.PROD,
+      },
+    })
+  );
 
   const documentConfig = new DocumentBuilder()
     .setTitle("Discord clone")
